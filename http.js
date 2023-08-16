09db4f801afeef42e4f42b9c46222de672bbada1
ari hlfgut <ari@0541234.com>
2023-08-16 14:25:08 +0300

    finish 1

--- http.js
+++ http.js
@@ -26,13 +26,44 @@ const users = [
   },
 ];
 const express = require("express");
-// const send = require("send");
 const app = express();
 const port = 3000;
-// const data = require("./data");
-// const fs = require("fs");
+
 const { v4: uuid } = require("uuid");
 app.use(express.json());
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+//---------------------------------//
+
+// const data = require("./data");
+// const fs = require("fs");
+
+// const hashMiddleware = (req, res, next) => {
+//   const { email, password } = req.body;
+//   const hash = bcrypt.hashSync(password, saltRounds);
+//   password = hash;
+//   next();
+// };
+
+// fs.writeFile("data.js", data, (err) => {
+//   if (err) throw err;
+//   console.
+// });
+
+//---------------------------------//
+
+function withoutEncryption(password, hash) {
+  const outHash = bcrypt.compareSync(password, hash);
+  return outHash;
+}
+
+function hash(password, saltRounds) {
+  const hash = bcrypt.hashSync(password, saltRounds);
+  return hash;
+}
+users.forEach((element) => {
+  element.password = hash(element.password, saltRounds);
+});
 
 app.get("/", (req, res) => {
   res.send(users);
@@ -53,8 +84,7 @@ app.post("/", (req, res) => {
   if (!email || !password) {
     return res.status(400).json({ error: "Missing required fields" });
   }
-
-  const newUser = { id: uuid(), email, password };
+  const newUser = { id: uuid(), email, password: hash(password, saltRounds) };
   users.push(newUser);
   res.send(users);
   log("The file has been saved!");
@@ -76,8 +106,34 @@ app.put("/:id", (req, res) => {
   log("The file has been saved!");
 });
 
-// fs.writeFile("data.js", data, (err) => {
-//   if (err) throw err;
-//   console.
-// });
+app.delete("/:id", (req, res) => {
+  const userId = req.params.id;
+  const index = users.findIndex((user) => user.id === userId);
+  const userSearch = users.find((user) => user.id === userId);
+  if (!userSearch) {
+    return res.status(404).json({ error: "User not found" });
+  }
+  users.splice(index, 1);
+  res.send(users);
+  return res.status(200).send(`User ${userId} delete`);
+});
+
+app.post("/Search", (req, res) => {
+  const { email, password } = req.body;
+  const index1 = users.findIndex((user) => user.email === email);
+  if (!email || !password) {
+    return res.status(400).json({ error: "Missing required fields" });
+  }
+  const userFound = users.find(
+    (element) =>
+      bcrypt.compareSync(password, element.password) && element.email === email
+  );
+
+  if (userFound) {
+    return res.send(`User found, ID:${users[index1].id}`);
+  } else {
+    return res.status(404).json({ error: "User not found" });
+  }
+});
+
 app.listen(port, () => console.log(`Server started on port ${port}`));
